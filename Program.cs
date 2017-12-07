0348956fc43f0a83bf48a22b22f311fc983daf2e
Andreas Larsson <andreas.larsson@bibliotekscentrum.se>
2017-12-07 16:49:22 +0100

    Day 7 - tricky part 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            int[] solvedDays = new int[] { 6 };
+            int[] solvedDays = new int[] { 7 };
 
             foreach (var day in solvedDays)
             {
