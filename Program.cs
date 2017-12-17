963926fd4807444c48b4f9b471a492764b3b1191
Andreas Larsson <andreas.larsson@bibliotekscentrum.se>
2017-12-17 10:33:18 +0100

    Day 17

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            int[] solvedDays = new int[] { 16 };
+            int[] solvedDays = new int[] { 17 };
 
             foreach (var day in solvedDays)
             {
--- solutions/Solution_17.cs
+++ solutions/Solution_17.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -8,18 +9,54 @@ namespace AdventOfCode.solutions
     {
         public static string getResult()
         {
-            var input = Helpers.getInputFromFile("day17.txt");
+            var input = 303;
+
+            Debug.Assert(getSolution(3) == 638);
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
             // Solution goes here
-            var solutionPart_1 = 1;
-            var solutionPart_2 = 2;
+            var solutionPart_1 = getSolution(input);
+            var solutionPart_2 = getSolution_2(input);
 
             sw.Stop();
 
             return $"Part 1: {solutionPart_1}{Environment.NewLine}Part 2: {solutionPart_2}{Environment.NewLine}Completion time: {sw.ElapsedMilliseconds}ms";
+        }
+
+        private static int getSolution(int steps)
+        {
+            List<int> vortex = new List<int>();
+            var currentPosition = 0;
+            vortex.Add(0);
+
+            for (int i = 1; i <= 2017; i++)
+            {
+                currentPosition = ((steps + currentPosition) % i) + 1;
+
+                vortex.Insert(currentPosition, i);
+            }
+
+            return vortex[vortex.IndexOf(2017) + 1];
+        }
+
+        private static int getSolution_2(int steps)
+        {
+            var currentPosition = 0;
+            var lReturn = 0;
+
+            for (int i = 1; i <= 50000000; i++)
+            {
+                currentPosition = ((steps + currentPosition) % i) + 1;
+
+                if (currentPosition == 1)
+                {
+                    lReturn = i;
+                }
+            }
+
+            return lReturn;
         }
     }
 }
